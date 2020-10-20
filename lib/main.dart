0dda06ed0a2fa43d9dbb4bc29ec46a9bb366f345
Tomasz Budny <tomasz.budny.pro@gmail.com>
2020-10-20 19:50:27 +0200

    refactor: Main._questions to be const

--- lib/main.dart
+++ lib/main.dart
@@ -14,9 +14,7 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  var _questionIndex = 0;
-
-  var _questions = [
+  static const _questions = [
     {
       'questionText': 'What\'s your favorite color',
       'answers': ['Black', 'Red', 'Green', 'White']
@@ -31,6 +29,8 @@ class _MyAppState extends State<MyApp> {
     },
   ];
 
+  var _questionIndex = 0;
+
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
@@ -39,10 +39,10 @@ class _MyAppState extends State<MyApp> {
         ),
         body: Column(
           children: [
-            Question(this._questions[this._questionIndex]['questionText']),
-            ...(this._questions[this._questionIndex]['answers'] as List<String>)
+            Question(_questions[this._questionIndex]['questionText']),
+            ...(_questions[this._questionIndex]['answers'] as List<String>)
                 .map((answer) {
-                  return Answer(this._answerQuestion, answer);
+              return Answer(this._answerQuestion, answer);
             }).toList()
           ],
         ),
@@ -57,12 +57,12 @@ class _MyAppState extends State<MyApp> {
     });
     print('Q index after: ' + this._questionIndex.toString());
 
-    if (this._questionIndex >= this._questions.length) {
+    if (this._questionIndex >= _questions.length) {
       setState(() {
         this._questionIndex = 0;
       });
       print('Q index reset: ' + this._questionIndex.toString());
     }
-    print('Question: ' + this._questions[this._questionIndex].toString());
+    print('Question: ' + _questions[this._questionIndex].toString());
   }
 }
