b705e0a511422b9b48f5d19c0074852fddcc2ed9
Tomasz Budny <tomasz.budny.pro@gmail.com>
2020-10-22 21:21:12 +0200

    feat: different result texts

--- lib/result.dart
+++ lib/result.dart
@@ -5,12 +5,31 @@ class Result extends StatelessWidget {
 
   Result(this.totalScore);
 
+  String get resultPhrase {
+    String resultText;
+    if (this.totalScore == 111) {
+      resultText = 'Could not be less dude!';
+    } else if (this.totalScore < 500) {
+      resultText = 'Try harder it\'s not that complicated';
+    } else if (this.totalScore < 400) {
+      resultText = 'Try harder it\'s not that complicated';
+    } else if (this.totalScore < 533) {
+      resultText = 'You are getting close';
+    } else if (this.totalScore == 534) {
+      resultText = 'Boom! MAX points!';
+    } else {
+      resultText = 'Some how you got out of the original scope =O';
+    }
+    return resultText;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Text(
-          'All done, bye! \n\n Score: ' + totalScore.toString(),
-          style: TextStyle(fontSize: 28),
+        this.resultPhrase + '\n\n Score: ' + totalScore.toString(),
+        style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+        textAlign: TextAlign.center,
       ),
     );
   }
